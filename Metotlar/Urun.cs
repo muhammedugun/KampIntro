7ba5492264303ed9bb27ad8bee9144a8e5b562bd
Muhammed <Muhammed@DESKTOP-R98UUIU>
2022-06-26 22:33:19 +0300

    Add project files.

---
+++ Metotlar/Urun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        public int Id { get; set; }
+        protected int bbc;
+       
+        
+    }
+
+    class Urun2:Urun
+    {
+       internal void eneme()
+        {
+            bbc = 5;
+        }
+    }
+    class Ana
+    {
+        static void Main()
+        {
+            Urun2 u = new Urun2();
+            u.eneme();
+        }
+    }
+
+}

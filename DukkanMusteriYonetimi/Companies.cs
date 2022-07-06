d800a3c7a946e2fc9010b29ff21a858309f122b3
0muhammed.ugun@gmail.com <0muhammed.ugun@gmail.com>
2022-07-06 13:23:56 +0300

    5. Hafta Gerçek Hayatta Interface ve Abstract Senaryosu

---
+++ DukkanMusteriYonetimi/Companies.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DukkanMusteriYonetimi
+{
+    class StarBucks : IBuyManager, IAwardManager, IRegisterVerificationManager, ICompaniesRegisterManager
+    {
+        public void Buy()
+        {
+            Console.WriteLine("Starbucks müşterisi satın alma gerçekleştirildi.");
+            EarnStar();
+        }
+
+        public void EarnStar()
+        {
+            Console.WriteLine("Starbucks müşterisi yıldız kazandı.");
+        }
+
+        public bool MernisVerification()
+        {
+            Console.WriteLine("Mernis doğrulaması gerçekleştiriliyor...");
+            return true;
+        }
+
+        public void RegisterCustomer()
+        {
+            Console.WriteLine("Starbucks müşterisi için kayıt işlemi başlatıldı.");
+            if (MernisVerification())
+                Console.WriteLine("Müşteri kaydı başarılı.");
+            else
+                Console.WriteLine("Müşteri kaydı başarısız.");
+
+        }
+    }
+
+    class Nero : IBuyManager, ICompaniesRegisterManager
+    {
+        public void Buy()
+        {
+            Console.WriteLine("Nero müşterisi satın alma gerçekleştirildi.");
+            
+        }
+
+        public void RegisterCustomer()
+        {
+            Console.WriteLine("Nero müşterisi için kayıt işlemi başlatıldı.");
+            Console.WriteLine("Müşteri kaydı başarılı.");
+
+
+        }
+    }
+}
---
+++ DukkanMusteriYonetimi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Kahve dükkanları için müşteri yönetimi yapan sistemin simulasyonu
+    Starbucks ve Nero firmaları olacak.
+    iki firmada müşterilerini veritabanına kaydetmek istiyor.
+    starbucks müşterilerini kaydederken mernis doğrulaması istiyor ama Nero firması istemiyor.
+    Starbucks müşterileri için her kahve alımında yıldız kazandırmak istiyor
+    
+*/
+namespace DukkanMusteriYonetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StarBucks starBucks = new StarBucks();
+            Nero nero = new Nero();
+
+            starBucks.RegisterCustomer();
+            starBucks.Buy();
+
+            nero.RegisterCustomer();
+            nero.Buy();
+
+            Console.ReadLine();
+        }
+    }
+}
